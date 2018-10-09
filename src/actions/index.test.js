2d045b2698a12cfd5b54f1277c6d36fdcdea5741
george <george.6809@gmail.com>
2018-10-09 16:16:22 +0900

    action と reducer のテストを作った

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -17,33 +17,69 @@ describe('Actions', () => {
     }
     expect(result).toEqual(expected)
   }),
-  it('timeOverTime Action', () => {
-    const time = 10
-    const result = actions.timeOverTime(time)
-    const expected = {
-      type: 'TIME_OVER',
-      payload: {
-        timeOver: time
-      }
-    }
-    expect(result).toEqual(expected)
-  }),
-  it('moveTimer Action', () => {
-
-
-    const expected = [
-      {
-        type: "STOP_TIMER",
+    it('timeOverTime Action', () => {
+      const time = 10
+      const result = actions.timeOverTime(time)
+      const expected = {
+        type: 'TIME_OVER',
         payload: {
-          timerId: null
+          timeOver: time
         }
       }
-    ]
-    const store = mockStore()
-    store.setState({timerId: null})
-    store.dispatch(actions.moveTimer())
-
-    expect(store.getActions()).toEqual(expected)
-
-  })
+      expect(result).toEqual(expected)
+    }),
+    it('moveStopTimer Action', () => {
+      const expected = [
+        {
+          type: 'STOP_TIMER',
+          payload: {
+            timerId: null
+          }
+        }
+      ]
+      const store = mockStore()
+      store.dispatch(actions.moveStopTimer())
+      expect(store.getActions()).toEqual(expected)
+    }),
+    it('clearTimer Action', () => {
+      const expected = [
+        {
+          type: 'RESET_TIMER'
+        }
+      ]
+      const store = mockStore()
+      store.dispatch(actions.clearTimer())
+      expect(store.getActions()).toEqual(expected)
+    }),
+    it('moveStartTimer Action', () => {
+      const expected = id => [
+        {
+          type: 'START_TIMER',
+          payload: {
+            timerId: id
+          }
+        }
+      ]
+      const store = mockStore()
+      store.dispatch(actions.moveStartTimer())
+      const { timerId } = store.getActions()[0].payload
+      expect(store.getActions()).toEqual(expected(timerId))
+    }),
+    it('ckeckTimer Action', () => {
+      const expected = [
+        {
+          type: 'TIME_OVER',
+          payload: {
+            timeOver: true
+          }
+        }
+      ]
+      const store = mockStore({
+        timerCounter: 10,
+        limitTime: 9,
+        timeOver: false
+      })
+      store.dispatch(actions.checkTimer())
+      expect(store.getActions()).toEqual(expected)
+    })
 })
